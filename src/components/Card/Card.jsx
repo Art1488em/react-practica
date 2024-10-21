919013b251e468dbbb3147b41a114236905a0a95
Art1488em <artemdavydov401@gmail.com>
2024-10-21 14:16:01 +0200

    Add files via upload

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Card.css'
+
+const Card = () => {
+  return (
+    <>
+    <div className="card">
+        <img src="" alt="" />
+        <h3>Lorem, ipsum dolor.</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, aperiam!</p>
+        <button>OK</button>
+
+    </div>
+    </>
+  )
+}
+
+export default Card
